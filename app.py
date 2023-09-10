0a9046259853d32987c1fe60ed1bb0cd4bb0e1af
Your Name <you@example.com>
2023-09-10 11:15:20 +0700

    add docker file

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+ 
+app = Flask(__name__)
+ 
+@app.route("/helloWorld", methods=["GET"])
+def helloWorld():
+    return 'Hello World, my name is Cuong'
+ 
+app.run(host="0.0.0.0", port=80, debug=True) 
